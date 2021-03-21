2afe364048005c8f0bbd8076789196bece13e99e
berinko <berin@live.com>
2021-03-21 23:39:46 +0100

    vikend projekat

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+function showTitle1() {
+    var x = document.getElementById("title1");
+    x.style.display = "block";
+}
+function hideTitle1() {
+    var x = document.getElementById("title1");
+    x.style.display = "none";
+}
